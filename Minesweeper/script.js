00da858f6bbb562d9627139b3cc0a51f5f2271dc
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-05-13 06:07:21 +0530

    workflow of app

---
+++ Minesweeper/script.js
@@ -0,0 +1,14 @@
+/*
+> we have to create a grid layout of 10x10 tiles.
+> in this grid we will have 10 mines. which will be randomly spread over the grid 
+> these mines will be revelead if we right click on them. 
+> once mine is revealed game will stop and there will be a message saying you lose.
+> near by tiles of mine will have numbers in it. which would show how close we are to a mine.
+> and there will be empty which when clicked. which when clicked will reveal all the near by empty tiles if any are 
+available.
+
+> i have to use ES6 modules.
+> i have to use the parcel.
+> i have to use npm init.
+
+*/
