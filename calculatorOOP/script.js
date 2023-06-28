9077e38d59b08fbe16eb08be9bdf48b1069b3178
Yogesh mehra <yogeshwebdev1@gmail.com>
2023-06-28 06:01:21 +0530

    oop calculator app

---
+++ calculatorOOP/script.js
@@ -0,0 +1,36 @@
+import { Calculator } from "./calculator.js";
+
+const calculatorGrid = document.querySelector(".calculator-grid");
+const primaryOperandDisplay = document.querySelector("[data-primary-operand]");
+const secondaryOperandDisplay = document.querySelector(
+  "[data-secondary-operand]"
+);
+const operationDisplay = document.querySelector("[data-operation]");
+
+const calculator = new Calculator(
+  primaryOperandDisplay,
+  secondaryOperandDisplay,
+  operationDisplay
+);
+
+calculatorGrid.addEventListener("click", (e) => {
+  if (e.target.matches("[data-all-clear]")) {
+    calculator.clear();
+  }
+
+  if (e.target.matches("[data-number]")) {
+    calculator.addDigit(e.target.textContent);
+  }
+
+  if (e.target.matches("[data-delete]")) {
+    calculator.removeDigit(e.target.textContent);
+  }
+
+  if (e.target.matches("[data-operation]")) {
+    calculator.chooseOperation(e.target.textContent);
+  }
+
+  if (e.target.matches("[data-equals]")) {
+    calculator.evaluate();
+  }
+});
